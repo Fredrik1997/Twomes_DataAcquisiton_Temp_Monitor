bbf90a793641e8bd41a6f693b69705dfec66f4d8
Fredrik <f.o.lautenbag@gmail.com>
2021-01-07 19:13:56 +0100

    build this to Wifi testing environment, used this for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,23 @@
-#include <esp_now.h>
 #include <WiFi.h>
 #include <Arduino.h>
+#include <identification.h>
+#include <HTTPClient.h>
 
-#define testPayload "{'id':'FF:FF:FF:FF:FF','dataSpec':{'lastTime':1606914671,'interval':10,'total':6},'data':{'pipeTemp1':[50.1,51.2,52.3,53.4,54.5],'pipeTemp2':[50.1,51.2,52.3,53.4,54.5]}}"
+#define testPayload "{\"id\":\"FF:FF:FF:FF:FF\",\"dataSpec\":{\"lastTime\":1606914671,\"interval\":10,\"total\":6},\"data\":{\"pipeTemp1\":[50.1,51.2,52.3,53.4,54.5],\"pipeTemp2\":[50.1,51.2,52.3,53.4,54.5]}}"
 #define length_testPayload 169
 
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; //REVIEW, this is a broadcast address
 
-typedef struct struct_message
-{
-  char sendMessage[length_testPayload + 1];
-} struct_message;
-
-struct_message myData;
-
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
-{
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
-
 #define timeMeasurePin 14
 
 char testMessage[] = testPayload;
+
+const char *ssid = userWiFiSSID;         //ssid internet access point, configure in utils.h
+const char *password = userWiFipassword; //password internet access point, configure in utils.h
+
+const char *serverName = userServerName;
+
+uint16_t serverPort = userServerPort;
 
 void setup()
 {
@@ -36,49 +31,49 @@ void setup()
   Serial.print("has lenght: ");
   Serial.println(strlen(testMessage));
 
-  strcpy(myData.sendMessage, testMessage);
-  Serial.print("myData.sendMessage: ");
-  Serial.println(myData.sendMessage);
-
-  WiFi.mode(WIFI_STA);
-
-  if (esp_now_init() != ESP_OK)
+  if (WiFi.status() != WL_CONNECTED)
   {
-    Serial.println("Error initializing ESP-NOW");
-    return;
+    WiFi.begin(ssid, password);
   }
 
-  //esp_now_register_send_cb(OnDataSent);
+  Serial.println("Connected");
+}
 
-  // Register peer
-  esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 6;
-  peerInfo.encrypt = false;
+boolean sendMessage()
+{
+  // Set values to send
+  HTTPClient http;
+  String httpRequestData = testPayload;
 
-  // Add peer
-  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  digitalWrite(timeMeasurePin, HIGH); //for time logging with scope
+  if (!http.begin(serverName, serverPort, "/set/house/centralHeatingTemperature"))
   {
-    Serial.println("Failed to add peer");
-    return;
+    return false;
   }
+  //http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httpResponseCode = http.POST(httpRequestData);
+  // Serial.print("httpRequestData : ");
+  // Serial.println(httpRequestData);
+
+  // Serial.print("HTTP Response code: ");
+  // Serial.println(httpResponseCode);
+
+  http.end();
+  digitalWrite(timeMeasurePin, LOW); //for time logging with scope
+
+  if (httpResponseCode == 200)
+  {
+    Serial.println("send succeed");
+    return true;
+  }
+  return false;
 }
 
 void loop()
 {
-  // Set values to send
 
-  digitalWrite(timeMeasurePin, HIGH); //for time logging with scope
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-  digitalWrite(timeMeasurePin, LOW); //for time logging with scope
+  sendMessage();
 
-  if (result == ESP_OK)
-  {
-    // Serial.println("Sent with success");
-  }
-  else
-  {
-    Serial.println("Error sending the data");
-  }
-  delay(10000);
+  delay(5000);
 }
